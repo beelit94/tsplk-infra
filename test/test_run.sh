37f705d68b4dd33326647fff6adc788a503990f9
beelit94 <beelit94@gmail.com>
2017-05-19 01:10:55 +0800

    1. update testing variables 2. support targeting modules

--- test/test_run.sh
+++ test/test_run.sh
@@ -3,15 +3,15 @@ user=$(cat test/salt_master_var.json | jq -r .'username')
 project=$(cat ../test/data/salt_master_var.json | jq -r .'project_name')
 aws_region=$(cat ../test/data/salt_master_var.json | jq -r .'aws_region')
 
-terraform apply -var="username=$user" -var="aws_region=$aws_region" tf/simple_bucket
+#terraform apply -var="username=$user" -var="aws_region=$aws_region" tf/simple_bucket
 
-mkdir -p tmp/salt_master
-cp -r tf/salt_master/* tmp/salt_master
-cd tmp/salt_master
+mkdir -p tmp
+cp -r tf/* tmp
+cd tmp
 
-terraform remote config -backend=s3 -backend-config="bucket=tsplk-bucket" \
--backend-config="key=$user-$project/salt_master.tfstate" \
+terraform init config -backend=s3 -backend-config="bucket=tsplk-bucket" \
+-backend-config="key=$user-$project/terraform.tfstate" \
 -backend-config="region=$aws_region"
-terraform apply -var-file=../../test/salt_master_var.json
+terraform plan -var-file=../../test/salt_master_var.json
 
 cd ../..
