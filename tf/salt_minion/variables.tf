f990adfd4d2408e96b87bd083c93ff8daaeab35b
beelit94 <beelit94@gmail.com>
2017-06-06 14:34:58 -0700

    change key names

--- tf/salt_minion/variables.tf
+++ tf/salt_minion/variables.tf
@@ -2,10 +2,10 @@
 variable "atlas_version" {
   type = "map"
   default = {
-    "ubuntu-1404" = "latest"
-    "windows-2008-r2" = "latest"
-    "windows-2012-r2" = "latest"
-    "salt-master" = "latest"
+    "ubuntu_1404" = "latest"
+    "windows_2008_r2" = "latest"
+    "windows_2012_r2" = "latest"
+    "salt_master" = "latest"
   }
 }
 variable "atlas_token" {}
@@ -20,10 +20,8 @@ variable "rdp_password" {
 }
 
 // tsplk master info ===========================
-variable "master_record_name" {
-}
-variable "key_pair_name" {
-}
+variable "master_record_name" {}
+variable "key_pair_name" {}
 
 // tpslk minion info ===========================
 // number of minion id as key, platform as value.
